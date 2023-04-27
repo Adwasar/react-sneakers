c3a7f3a0e89fcba4eb11343e5ec0dbe5bee0edd4
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-04-27 12:31:40 +0300

    feat: test routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
@@ -44,71 +45,81 @@ function App() {
         />
       )}
       <Header onClickCart={() => setCartOpened(true)} />
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>
-            {searchValue ? `Поиск по: "${searchValue}"` : "Все кроссовки"}
-          </h1>
-          <div className="search-block d-flex align-center">
-            <img height={16} width={16} src="img/search.svg" alt="Search" />
-            <input
-              onChange={handleChangeSearch}
-              value={searchValue}
-              placeholder="Поиск..."
-            />
-            {searchValue && (
-              <img
-                onClick={() => setSearchValue("")}
-                className="close-btn"
-                src="/img/btn-remove.svg"
-                alt="Close"
+      <main>
+        <Routes>
+          <Route path="/" element={
+            <p>home page</p>
+          } />
+          <Route path="/favorites" element={ 
+            <p>favorites page</p>
+          } />
+        </Routes>
+        <div className="content p-40">
+          <div className="d-flex align-center justify-between mb-40">
+            <h1>
+              {searchValue ? `Поиск по: "${searchValue}"` : "Все кроссовки"}
+            </h1>
+            <div className="search-block d-flex align-center">
+              <img height={16} width={16} src="img/search.svg" alt="Search" />
+              <input
+                onChange={handleChangeSearch}
+                value={searchValue}
+                placeholder="Поиск..."
               />
-            )}
+              {searchValue && (
+                <img
+                  onClick={() => setSearchValue("")}
+                  className="close-btn"
+                  src="/img/btn-remove.svg"
+                  alt="Close"
+                />
+              )}
+            </div>
+          </div>
+
+          <div className="d-flex flex-wrap">
+            {items
+              .filter((item) =>
+                item.title.toLowerCase().includes(searchValue.toLowerCase())
+              )
+              .map((obj, i) => {
+                obj.key = i;
+
+                const onClickPlus = async () => {
+                  const isItemOnServer = addedItems.filter(
+                    (el) => el.title === obj.title && el.image === obj.image
+                  );
+
+                  if (!isItemOnServer.length) {
+                    await axios.post(
+                      "https://64020cd7ab6b7399d0b2a6df.mockapi.io/cart",
+                      obj
+                    );
+                  } else {
+                    const id = isItemOnServer[0].id;
+                    deleteItem(id);
+                  }
+
+                  await axios
+                    .get("https://64020cd7ab6b7399d0b2a6df.mockapi.io/cart")
+                    .then((res) => setBackAddedItems(res.data));
+                };
+
+                return (
+                  <Card
+                    key={i}
+                    item={obj}
+                    title={obj.title}
+                    price={obj.price}
+                    image={obj.image}
+                    addedItems={addedItems}
+                    onClickPlus={onClickPlus}
+                  />
+                );
+              })}
           </div>
         </div>
-
-        <div className="d-flex flex-wrap">
-          {items
-            .filter((item) =>
-              item.title.toLowerCase().includes(searchValue.toLowerCase())
-            )
-            .map((obj, i) => {
-              obj.key = i;
-
-              const onClickPlus = async () => {
-                const isItemOnServer = addedItems.filter(
-                  (el) => el.title === obj.title && el.image === obj.image
-                );
-
-                if (!isItemOnServer.length) {
-                  await axios.post(
-                    "https://64020cd7ab6b7399d0b2a6df.mockapi.io/cart",
-                    obj
-                  );
-                } else {
-                  const id = isItemOnServer[0].id;
-                  deleteItem(id);
-                }
-
-                await axios
-                  .get("https://64020cd7ab6b7399d0b2a6df.mockapi.io/cart")
-                  .then((res) => setBackAddedItems(res.data));
-              };
-
-              return (
-                <Card
-                  key={i}
-                  item={obj}
-                  title={obj.title}
-                  price={obj.price}
-                  image={obj.image}
-                  addedItems={addedItems}
-                  onClickPlus={onClickPlus}
-                />
-              );
-            })}
-        </div>
-      </div>
+      </main>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,44 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
-      <div className="headerLeft d-flex align-center">
-        <img className="logo" width={40} height={40} src="img/logo.png" alt="logo" />
-        <div className="headerInfo">
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
+      <Link to="/">
+        <div className="headerLeft d-flex align-center cu-p">
+          <img
+            className="logo"
+            width={40}
+            height={40}
+            src="img/logo.png"
+            alt="logo"
+          />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
         </div>
-      </div>
-      <ul className="headerRight d-flex">
-        <li onClick={props.onClickCart} className="d-flex align-center mr-25 cu-p">
-          <img src="img/cart.svg" width={20} height={20} alt="Корзина" />
-          <span className="ml-5">1205 $</span>
-        </li>
-        <li className="d-flex align-center mr-25 cu-p">
-          <img src="img/heart.svg" width={20} height={20} alt="Закладки" />
-        </li>
-        <li className="d-flex align-center cu-p">
-          <img src="img/user.svg" width={22} height={22} alt="Пользователь" />
-        </li>
-      </ul>
+      </Link>
+      <nav>
+        <ul className="headerRight d-flex">
+          <li
+            onClick={props.onClickCart}
+            className="d-flex align-center mr-25 cu-p"
+          >
+            <img src="img/cart.svg" width={20} height={20} alt="Корзина" />
+            <span className="ml-5">1205 $</span>
+          </li>
+          <Link to="/favorites">
+            <li className="d-flex align-center mr-25 cu-p">
+              <img src="img/heart.svg" width={20} height={20} alt="Закладки" />
+            </li>
+          </Link>
+          <li className="d-flex align-center cu-p">
+            <img src="img/user.svg" width={22} height={22} alt="Пользователь" />
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
